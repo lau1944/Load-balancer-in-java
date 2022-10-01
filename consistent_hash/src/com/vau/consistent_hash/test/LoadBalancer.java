700c8f9d45f6db47fc63a836483de1126d2c1900
lau1944 <a954723219@gmail.com>
2022-10-01 17:10:53 -0400

    feat: add virutal node test

--- consistent_hash/src/com/vau/consistent_hash/test/LoadBalancer.java
+++ consistent_hash/src/com/vau/consistent_hash/test/LoadBalancer.java
@@ -3,16 +3,21 @@ package com.vau.consistent_hash.test;
 import com.vau.consistent_hash.INode;
 import com.vau.consistent_hash.Node;
 import com.vau.consistent_hash.RouteTable;
+import com.vau.consistent_hash.VirtualNode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public final class LoadBalancer {
     public static final void main(String[] args) {
-        List<INode> nodes = List.of(new INode[]{
-            new Node("127.0.0.1", "80"), new Node("127.0.0.2", "80"),
-                new Node("127.0.0.3", "80"), new Node("127.0.0.4", "80")
-
-        });
+        Node node = new Node("127.0.0.1", "80");
+        List<INode> nodes = Arrays.asList(new Node("127.0.0.1", "80"), new Node("127.0.0.2", "80"),
+                new Node("127.0.0.3", "80"), new Node("127.0.0.4", "80"),
+                new VirtualNode(node, 0),
+                new VirtualNode(node, 1),
+                new VirtualNode(node, 2),
+                new VirtualNode(node, 3));
         RouteTable table = new RouteTable(nodes);
 
         // get random server
